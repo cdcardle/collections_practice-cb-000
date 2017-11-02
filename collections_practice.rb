94eeeb59387a4cc22a166b6cb0d21083d367a723
Chris Cardle <cdcardle@gmail.com>
2017-11-02 13:26:24 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -38,5 +38,5 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index.collect {|e, i| i !== 2 ? array[i] = "#{e}s"}
+  array.each_with_index.collect {|e, i| i !== 2 ? array[i] = "#{e}s" : }
 end
